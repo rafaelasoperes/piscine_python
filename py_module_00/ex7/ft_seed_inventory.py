045c2c4f62249ca5700ae760cad98d9e5e706936
rafasilv <rafaelasoperes@gmail.com>
2026-03-26 17:36:01 -0300

    Ajustando funções

--- py_module_00/ex7/ft_seed_inventory.py
+++ py_module_00/ex7/ft_seed_inventory.py
@@ -5,7 +5,10 @@ def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
         print(f"{seed_type.capitalize()} seeds: {quantity} grams total")
     elif unit == "area":
         print(f"{seed_type.capitalize()} seeds: covers {quantity} square meters")
+    else:
+        print("Unknown unit type")
 
 ft_seed_inventory("tomato", 15, "packets")
 ft_seed_inventory("carrot", 8, "grams")
 ft_seed_inventory("lettuce", 12, "area")
+ft_seed_inventory("lettuce", 12, "null")
