106ff51e2f9bbb43b2cf11182e1fecf0c5c1a8a3
rafasilv <rafaelasoperes@gmail.com>
2026-04-10 12:03:00 -0300

    .

--- py_module_01/ex0/ft_garden_intro.py
+++ py_module_01/ex0/ft_garden_intro.py
@@ -1,14 +1,15 @@
 def main():
     name = "Manjericão"
     height = 12.5
-    age = 3 
+    age = 3
 
     print("=== Welcome to my Garden ===")
     print(f"Plant {name}")
     print(f"Height: {height}cm")
     print(f"Age: {age} days")
     print("\n")
-    print(f"=== End of Program ===")
+    print("=== End of Program ===")
+
 
 if __name__ == "__main__":
     main()
