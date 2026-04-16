e95ae0a01ec478505b010f02d791cbba152695e3
rafasilv <rafaelasoperes@gmail.com>
2026-04-16 08:30:07 -0300

    Esta errado

--- py_module_03/ex1/ft_score_analytics.py
+++ py_module_03/ex1/ft_score_analytics.py
@@ -6,14 +6,16 @@ def main() -> None:
     count: int = len(sys.argv)
     if (count > 1):
         i: int = 1
-        list_arg = ""
+        list_arg: str = ""
         while (count > i):
             list_arg += sys.argv[i]
             if i < count - 1:
                 list_arg += ", "
             i += 1
-
         print(f"Scores processed: [{list_arg}]")
+        print(f"Total players: {count - 1}")
+        totalScore = sum(sys.argv)
+        print(f"Total score: {totalScore}")
     else:
         print(f"No scores provided. Usage: python3 {sys.argv[0]} <score1> "
               "<score2> ...")
